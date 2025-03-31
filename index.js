6a7777fd1b9dd2557ab6902781bb853a3757a192
hexique <69116911+hexique@users.noreply.github.com>
2025-03-31 20:01:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
-const thisYear = new Date().getFullYear()
-const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
-const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
-const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
 const progressBarOfThisYear = generateProgressBar()
 
-function generateProgressBar() {
-    const progressBarCapacity = 30
-    const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
-    const progressBar =
-      '█'.repeat(passedProgressBarIndex) +
-      '▁'.repeat(progressBarCapacity - passedProgressBarIndex)
-    return `{ ${progressBar} }`
+function generateProgressBar(len) {
+  const thisYear = new Date().getFullYear()
+  const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
+  const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
+  const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
+  const passedProgressBarIndex = parseInt(progressOfThisYear * len)
+  const progressBar =
+    '█'.repeat(passedProgressBarIndex) +
+    '▁'.repeat(len - passedProgressBarIndex)
+  return `${parseFloat(progressOfThisYear * 100)}% { ${progressBar} }<br>${passedProgressBarIndex}/${len}`
 }
 
-const readme = `\
-### Hi there 👋
+function displayPrograssBar() {
+  document.querySelectorAll('p')[0].innerHTML = generateProgressBar(parseInt(document.querySelectorAll("input")[0].value));
+}
 
-⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
-
----
-
-⏰ Updated on ${new Date().toUTCString()}
-
-![Progress Bar CI](https://github.com/liununu/liununu/workflows/Progress%20Bar%20CI/badge.svg)\
-`
-
-console.log(readme)
+document.querySelectorAll("input")[0].value = "52"
+document.querySelectorAll('p')[0].textContent = generateProgressBar();
